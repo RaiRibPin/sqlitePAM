ac75a79805c8dce1feb8b26605098d7d9e13aa2f
RaiRibPin <raissarp470717@gmail.com>
2025-04-14 20:54:16 -0300

    Update criarTabela e inserirDados

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Banco from './banco.tsx';
 
 export default function App() {
--- banco.tsx
+++ banco.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, StyleSheet } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
 // componentes
@@ -12,12 +12,48 @@ const Banco = () =>{
         }else{
             console.log('Erro');
         }
+        return db;
+    }
+    async function criarTabela () {
+        try{
+            let db = await criaDatabase();
+            await db.execAsync(`PRAGMA journal_mode = WAL;
+                CREATE TABLE IF NOT EXISTS tb_usuario (id INTEGER PRIMARY KEY NOT NULL, nome TEXT NOT NULL);`);
+                console.log('Tabela criada');
+        } catch(error) {
+            console.log('Erro ao criar tabela', error);
+        }
+    }
+    async function inserirDados () {
+        try{
+            let db = await criaDatabase();
+            await db.execAsync(`PRAGMA journal_mode = WAL;
+                CREATE TABLE IF NOT EXISTS tb_usuario (id INTEGER PRIMARY KEY NOT NULL, nome TEXT NOT NULL);`);
+                console.log('Tabela criada');
+        } catch(error) {
+            console.log('Erro ao criar tabela', error);
+        }
     }
     return(
         <View>
-            <Button onPress={criaDatabase} title="Create Banco" />
+            <View style={style.button}>
+                <Button onPress={criaDatabase} title="Criar Banco" />
+            </View>
+            <View style={style.button}>
+                <Button onPress={criarTabela} title="Criar tabela"/>
+            </View>
+            <View style={style.button}>
+                <Button onPress={inserirDados} title="Inserir dados"/>
+            </View>
         </View>
+        
     )
 }
+
+const style = StyleSheet.create({
+    button:{
+        marginBottom: 20,
+    }
+})
 
 export default Banco;
